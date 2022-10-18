91076f3bce2b59d37dfc498bdb19fc3574fee9e9
Compass <ndphong165@gmail.com>
2022-10-18 14:59:44 +0700

    done TH1, customer thymeleaf

---
+++ Practice/customer-management-thymleaf/src/main/java/customermanagementthymleaf/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package customermanagementthymleaf.service;
+
+import customermanagementthymleaf.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
